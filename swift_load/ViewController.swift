bbe19040449a229c49afc2b05b7884394337e3b9
蜂尚网络 <fengshang02@fengshangwangluodeMac-mini.local>
2018-03-20 14:24:56 +0800

    Initial Commit

---
+++ swift_load/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift_load
+//
+//  Created by 蜂尚网络 on 2018/3/20.
+//  Copyright © 2018年 王绵峰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
